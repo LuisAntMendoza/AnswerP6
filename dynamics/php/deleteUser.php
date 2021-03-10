f92dcf7ea862b43cadf1f81bb634d6d941ed9bbc
LuisAntMendoza <mendozaluis@comunidad.unam.mx>
2021-03-09 19:29:23 -0600

    FIX: error al borrar un usuario desde admin

--- dynamics/php/deleteUser.php
+++ dynamics/php/deleteUser.php
@@ -5,7 +5,9 @@ include("./config.php");
 $conexion = conectarBD();
 
 //borra el usuario de la BD
-$consulta = 'DELETE FROM usuario WHERE id_usuario = "'.$_POST['idUser'].'"';
+$consulta = 'DELETE FROM encuestas_respondidas WHERE id_usuario = '.$_POST['idUser'];
+$consultar = mysqli_query($conexion, $consulta);
+$consulta = 'DELETE FROM usuario WHERE id_usuario = '.$_POST['idUser'];
 $consultar = mysqli_query($conexion, $consulta);
 
 echo json_encode($consultar);
