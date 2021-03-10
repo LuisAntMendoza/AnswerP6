025d0442b51354e02c0d300fe5af1443bf5c6ffe
LuisAntMendoza <mendozaluis@comunidad.unam.mx>
2021-03-10 09:12:53 -0600

    FIX: recargar página al borrar un reporte

--- dynamics/php/deleteReporte.php
+++ dynamics/php/deleteReporte.php
@@ -1,14 +1,12 @@
 <?php
-//inciamos sesion y conectamos a la BD
+//iniciamos sesión y conectamos a la BD
 session_start();
 include("config.php");
 $conexion = conectarBD();
 
 //borramos el reporte y regresamos a admin
-$consulta = 'DELETE FROM reporte WHERE id_reporte = "'.$_POST['idReporte'].'"';
+$consulta = 'DELETE FROM reporte WHERE id_reporte = '.$_POST['idReporte'];
 $consultar = mysqli_query($conexion, $consulta);
 
-header("location: ../../templates/admin.php");
-
-
+echo $consultar;
  ?>
